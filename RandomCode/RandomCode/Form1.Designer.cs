8d5761f4cc6b9bb15eb679ec0e8dc046899f6c49
Zhifeng Lv <lvzhifeng2013@163.com>
2019-10-22 16:12:25 +0800

    Add files via upload

---
+++ RandomCode/RandomCode/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RandomCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(79, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(79, 177);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "获取验证码";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 258);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ RandomCode/RandomCode/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomCode
+{
+    public partial class Form1 : Form
+    {
+        private string checkCode;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkCode = GetRandomCode(4);//获取4个随机的数字或字母
+            CreateImage(checkCode);//实现验证码图片
+
+        }
+
+        private string GetRandomCode(int CodeCount)
+        {
+            string allChar = "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,i,J,K,M,N,P,Q,R,S,T,U,W,X,Y,Z";
+            string[] allCharArray = allChar.Split(',');
+            string RandomCode = "";
+            int temp = -1;
+
+            Random rand = new Random();
+            for (int i = 0; i < CodeCount; i++)
+            {
+                if (temp != -1)
+                {
+                    rand = new Random(temp * i * ((int)DateTime.Now.Ticks));
+                }
+
+                int t = rand.Next(33);
+
+                while (temp == t)
+                {
+                    t = rand.Next(33);
+                }
+
+                temp = t;
+                RandomCode += allCharArray[t];
+            }
+
+            return RandomCode;
+        }
+        private void CreateImage(string checkCode)
+        {
+            int iwidth = (int)(checkCode.Length * 14);
+            System.Drawing.Bitmap image = new System.Drawing.Bitmap(iwidth, 21);
+            Graphics g = Graphics.FromImage(image);
+            Font f = new System.Drawing.Font("Arial ", 10);
+            Brush b = new System.Drawing.SolidBrush(Color.Black);
+            Brush r = new System.Drawing.SolidBrush(Color.FromArgb(166, 8, 8));
+
+            g.Clear(System.Drawing.ColorTranslator.FromHtml("#99C1CB"));//背景色
+
+            char[] ch = checkCode.ToCharArray();
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i] >= '0' && ch[i] <= '9')
+                {
+                    //数字用红色显示
+                    g.DrawString(ch[i].ToString(), f, r, 3 + (i * 12), 3);
+                }
+                else
+                {   //字母用黑色显示
+                    g.DrawString(ch[i].ToString(), f, b, 3 + (i * 12), 3);
+                }
+            }
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            pictureBox1.Image = image;
+        }
+     
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            checkCode = GetRandomCode(4);
+            CreateImage(checkCode);
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(checkCode);
+        }
+    }
+}
